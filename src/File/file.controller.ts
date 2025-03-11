c2d43f71c8f4f222d8ce8f995f81b7dab03b735e
LiashenkoMA <makcim_an@mail.ru>
2025-03-11 19:52:10 +0400

    feat: апи добавления картинок

---
+++ src/File/file.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  FileTypeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+@Controller('images')
+export class FileController {
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const name = file.originalname.split('.')[0];
+          const fileExtension = file.originalname.split('.')[1];
+          const newFileName =
+            name.split(' ').join('_') + '_' + Date.now() + '.' + fileExtension;
+
+          cb(null, newFileName);
+        },
+      }),
+    }),
+  )
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: 'image|jpeg|png' })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    const filePath = {
+      filePath: `${process.env.URL_UPLOADIMG}/images/upload/${file.filename}`,
+    };
+    return filePath;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './User/user.module';
 import { AuthModule } from './Auth/auth.module';
+import { FileModule } from './File/file.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/'),
     UserModule,
     AuthModule,
+    FileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
